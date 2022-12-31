3d049f4d09696845aa5a9f19a9b1544178ec4e6c
github_aqsayasmeen <aqsa150282@gmail.com>
2022-12-31 19:38:54 +0500

    "Done"

--- pets/src/components/S4Q1.js
+++ pets/src/components/S4Q1.js
@@ -2,12 +2,12 @@ import axios from 'axios'
 import { useState } from 'react';
 // import './App.css'
 function App(){
-  const [shown,setShown]=useState(false)
+  // const [shown,setShown]=useState(false)
   const [data,setData]=useState([])
-  const[loc,setLoc]=useState("")
+  // const[loc,setLoc]=useState("")
   const test=()=>{
     try{
-    let res=axios.get(`http://api.weatherapi.com/v1/current.json?key=d3cc508bb4f24e2c99684107221012&q=${loc}`)
+    let res=axios.get(`https://learnwebcode.github.io/json-example/animals-1.json`)
     .then((response)=>setData(response.data));
   }
     catch{
@@ -21,19 +21,19 @@ function App(){
    <h1>PETS</h1>
         {/* <input type={'search'} value={loc} onChange={(e)=>setLoc(e.target.value)}/> */}
    <button className='button' onClick={test}>click me</button>
-   <br/>
+   {/* <br/>
    <button className='button' onClick={()=>setShown(true)}> show result</button>{
-    shown==true?
+    shown==true? */}
     <>
-    <p>{data.location.country}</p>
+    {/* <p>{data.location.country}</p>
     <p>{data.location.region}</p>
     <img src={data.current.condition.icon} alt="Loading Error" />
         <p>{data.current.condition.text}</p>
-        <p>{data.current.temp_c}  </p> 
+        <p>{data.current.temp_c}  </p>  */}
     </>
     :null
     
-   }
+   {/* } */}
 
    </div>
    </>
